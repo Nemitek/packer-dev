98c2d4745997952f6e8ed2246be5c23a41e4939e
aiur <mkotlikov@gmail.com>
2014-12-20 17:37:29 -0800

    Working script with automatic guest addition installation.

--- scripts/install_scala_sbt.sh
+++ scripts/install_scala_sbt.sh
@@ -1,7 +1,7 @@
 # scala install
 wget www.scala-lang.org/files/archive/scala-2.11.4.deb
-sudo dpkg -i scala-2.11.1.deb
+sudo dpkg -i scala-2.11.4.deb
 
 # sbt installation
-wget wget http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt/0.13.1/sbt.deb
+wget http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt/0.13.1/sbt.deb
 sudo dpkg -i sbt.deb
