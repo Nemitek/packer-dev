3c1378693e637573e3d54008172358bcb8080f9b
aiur <mkotlikov@gmail.com>
2015-01-17 17:57:57 -0800

    Created a nemitek user for development.

--- scripts/vagrant_bootstrap.sh
+++ scripts/vagrant_bootstrap.sh
@@ -2,7 +2,9 @@
 # Disable sudo password for vagrant
 sudo echo 'vagrant ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 
-sudo useradd -d /home/vagrant -m -p vagrant vagrant
+sudo useradd -d /home/nemitek -m nemitek
+echo nemitek | passwd nemitek --stdin
+sudo echo 'nemitek ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 
 # Grab vagrant public key and create related files
 sudo wget https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -P /tmp
